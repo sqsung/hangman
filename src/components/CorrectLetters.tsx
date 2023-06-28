da8b90aaba07484d3ded86e69a112d92ef1d348c
Kyu Sung Sohn <rok.ksohn@gmail.com>
2023-06-28 12:53:14 +0900

    Feat: CorrectLetters component

--- src/components/CorrectLetters.tsx
+++ src/components/CorrectLetters.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
 const CorrectLetters = () => {
-  return <div>Correct Letters</div>;
+  const word = 'test';
+  const guessedLetters = ['t'];
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        gap: '.25em',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace',
+      }}
+    >
+      {word.split('').map((letter, idx) => (
+        <span
+          key={idx}
+          style={{
+            borderBottom: '.1em solid black',
+          }}
+        >
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden',
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default CorrectLetters;
